288ebb73e048dc013bb203f0556339bad140d48a
𝗜𝗦𝗧𝗞𝗛𝗔𝗥 𝗕𝗔𝗕𝗬 <mdistkhar601@gmail.com>
2025-11-22 08:28:20 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,128 @@
+import re
+from os import getenv
+
+from dotenv import load_dotenv
+from pyrogram import filters
+
+load_dotenv()
+
+# Get this value from my.telegram.org/apps
+API_ID = int(getenv("API_ID","18996880"))
+API_HASH = getenv("API_HASH","4fbf3bdd35495f210e14b54d4588688d")
+
+# Get your token from @BotFather on Telegram.
+BOT_TOKEN = getenv("BOT_TOKEN","5907277199:AAEbkwvnRPugD5jxbkhE_S-m5U1VBQx0I28")
+
+# Get your mongo url from cloud.mongodb.com
+MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://yaemiko:yaemiko69@cluster0.ojfsgey.mongodb.net/?retryWrites=true&w=majority")
+
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 200))
+
+# Chat id of a group for logging bot's activities
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID","-1002222604638"))
+
+# Get this value from @MissRose_Bot on Telegram by /id
+OWNER_ID = int(getenv("OWNER_ID","6391774843"))
+
+# Your heroku app name
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME","mooncodes")
+# Get it from http://dashboard.heroku.com/account
+HEROKU_API_KEY = getenv("HEROKU_API_KEY","HRKU-AAkvMusLkK6RJlWLiEDJqEfDHwpHHNRwWSeZaHdIG6Qg_____wAqt48m8b7K")
+
+UPSTREAM_REPO = getenv(
+    "UPSTREAM_REPO",
+    "https://github.com/mooncodessssss/mooncodes",
+)
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+
+API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
+API_KEY = getenv("API_KEY", 'NxGBNexGenBotsb1880e') # youtube song api ke  -------------------------------------------------------------
+
+GIT_TOKEN = getenv(
+    "GIT_TOKEN", "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"
+)  # Fill this variable if your upstream repository is private
+
+SUPPORT_CHANNEL = getenv("SUPPORT_CHAT", "https://t.me/world_friend_chatting_zone")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/world_friend_chatting_zone")
+
+# Set this to True if you want the assistant to automatically leave chats after an interval
+AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
+
+# make your bots privacy from telegra.ph and put your url here 
+PRIVACY_LINK = getenv("PRIVACY_LINK", "https://files.catbox.moe/jyeumn.jpg")
+
+
+# Get this credentials from https://developer.spotify.com/dashboard
+SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
+SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
+
+
+# Maximum limit for fetching playlist's track from youtube, spotify, apple links.
+PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
+
+
+# Telegram audio and video file size limit (in bytes)
+TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600))
+TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 2145386496))
+# Checkout https://www.gbmb.org/mb-to-bytes for converting mb to bytes
+
+
+# Get your pyrogram v2 session from Replit
+STRING1 = getenv("STRING_SESSION","BQHGZiYAa6K-R1b7v5gk3irUqlHlGA0kRW1irTlvbqtS8AFnmdB6zcULOlArBVRsNgNJsR5cQOIzmHYTrjr1tD0-OnKD35A9hEhXlE0yqHJS_WS4pEAiFM_h_g2K06c4w2qRMSck7qX2g7zX7f95CeeHaVjdbDhAl6YaAJd9sU8Vq2h4EF41yMqFxBxkbZcOWpyxEGeMMqNTd7JIVnbLDgd_EVQp0M_MnEvXRAx2F3dJMsHyxCB2lh8hx3y-fUYvjz3J3M7MwzXhsuAt9Nw6hyGCaEmKBSaQ7ygaVJzctQuBioOcxL4JFsgyrIKq06UU6RvnVq6ArhZbfTeNmuRrepmD74k8wwAAAAHc8sT8AA")
+STRING2 = getenv("STRING_SESSION2", None)
+STRING3 = getenv("STRING_SESSION3", None)
+STRING4 = getenv("STRING_SESSION4", None)
+STRING5 = getenv("STRING_SESSION5", None)
+
+
+BANNED_USERS = filters.user()
+adminlist = {}
+lyrical = {}
+votemode = {}
+autoclean = []
+confirmer = {}
+
+
+START_IMG_URL = getenv(
+    "START_IMG_URL", "https://i.ibb.co/k3cw8XS/IMG-20250103-174108-968.jpg"
+)
+PING_IMG_URL = getenv(
+    "PING_IMG_URL", "https://i.ibb.co/h9XdzGp/IMG-20250103-174105-243.jpg"
+)
+PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
+STATS_IMG_URL = "https://i.ibb.co/h9XdzGp/IMG-20250103-174105-243.jpg"
+TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
+TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
+STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
+SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
+YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+
+
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(int(x) * 60**i for i, x in enumerate(reversed(stringt.split(":"))))
+
+
+DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
+
+
+if SUPPORT_CHANNEL:
+    if not re.match("(?:http|https)://", SUPPORT_CHANNEL):
+        raise SystemExit(
+            "[ERROR] - Your SUPPORT_CHANNEL url is wrong. Please ensure that it starts with https://"
+        )
+
+if SUPPORT_GROUP:
+    if not re.match("(?:http|https)://", SUPPORT_GROUP):
+        raise SystemExit(
+            "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
+        )
+
+
+
+
+
+
