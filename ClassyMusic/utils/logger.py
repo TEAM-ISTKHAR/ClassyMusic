4822ca7f1226693d51785076c4b90181630abefc
𝗜𝗦𝗧𝗞𝗛𝗔𝗥 𝗕𝗔𝗕𝗬 <mdistkhar601@gmail.com>
2025-11-22 08:34:30 +0530

    Add files via upload

---
+++ ClassyMusic/utils/logger.py
@@ -0,0 +1,58 @@
+from pyrogram import Client, enums, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, ChatPermissions, Message
+from ClassyMusic import app
+from ClassyMusic.misc import SUDOERS
+import asyncio
+from pyrogram import Client, filters, enums
+from pyrogram.types import Message
+from ClassyMusic import app, Userbot
+from ClassyMusic.utils.database import get_assistant
+from pyrogram.errors import UserAlreadyParticipant, UserNotParticipant, ChatAdminRequired
+from pyrogram.types import Message, ChatPrivileges
+import asyncio
+from typing import Optional
+from random import randint
+from pyrogram.raw.types import InputGroupCall, InputPeerChannel, InputPeerChat
+from pyrogram.enums import ParseMode
+from ClassyMusic import app
+from ClassyMusic.utils.database import is_on_off
+from config import LOG_GROUP_ID as LOG_GROUP_ID
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.enums import MessageEntityType
+from pyrogram.types import Message, User, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+import random
+
+
+async def play_logs(message, streamtype):
+    if await is_on_off(2):
+        chat_members = await app.get_chat_members_count(message.chat.id)
+        async for admin in app.get_chat_members(message.chat.id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
+            if admin.status == enums.ChatMemberStatus.OWNER:
+                owner_AMBOT = admin.user.mention if hasattr(admin.user, 'mention') and admin.user.mention else "Is_Hide / Deleted"
+                owner_AMBOT_id = admin.user.id if hasattr(admin.user, 'id') else "Is_Hide / Deleted"
+        logger_text = f"""
+<b>{app.mention} ᴘʟᴀʏ ʟᴏɢ</b>
+╔════❰𝐏𝐋𝐀𝐘𝐈𝐍𝐆❱═══❍⊱❁۪۪
+<b>◈ 𝐂𝐡𝐚𝐭 ➪ </b>{message.chat.title}
+<b>◈ 𝐂𝐡𝐚𝐭 𝐈𝐝 ➪ </b> <code>{message.chat.id}</code>
+<b>◈ 𝐔𝐬𝐞𝐫 ➪ </b> {message.from_user.mention}
+<b>◈ 𝐔𝐬𝐞𝐫𝐧𝐚𝐦𝐞 ➪ </b> @{message.from_user.username}
+<b>◈ 𝐈𝐝 ➪ </b> <code>{message.from_user.id}</code>
+<b>◈ 𝐂𝐡𝐚𝐭 𝐋𝐢𝐧𝐤 ➪ </b> @{message.chat.username}
+<b>◈ 𝐂𝗵𝗮𝘁 𝗠𝗲𝗺𝗯𝗲𝗿𝘀 ➪ </b> <code>{chat_members}</code>
+<b>◈ 𝐂𝗵𝗮𝘁 𝗢𝘄𝗻𝗲𝗿 ➪ </b> {owner_AMBOT} 𝐈𝐝 ➪<code>{owner_AMBOT_id}</code>
+<b>◈ 𝐒𝐞𝐚𝐫𝐜𝐡𝐞𝐝 ➪ </b> <code>{message.text.split(None, 1)[1]}</code>
+<b>◈ 𝐁𝐲 ➪ </b> {streamtype}
+╚═══❰ #𝐍𝐞𝐰𝐒𝐨𝐧𝐠 ❱══❍⊱❁۪۪"""
+        if message.chat.id != LOG_GROUP_ID:
+            try:
+                await app.send_message(
+                    chat_id=LOG_GROUP_ID,
+                    text=logger_text,
+                    parse_mode=ParseMode.HTML,
+                    disable_web_page_preview=True,
+                )
+            except:
+                pass
+        return
---
+++ ClassyMusic/utils/thumbnails.py
@@ -0,0 +1,135 @@
+import os
+import re
+import random
+import aiofiles
+import aiohttp
+import random
+import requests
+import os
+from PIL import Image, ImageEnhance, ImageFilter, ImageOps, ImageDraw, ImageFont
+from unidecode import unidecode
+from youtubesearchpython.__future__ import VideosSearch
+from ClassyMusic import app
+from config import YOUTUBE_IMG_URL
+
+def changeImageSize(maxWidth, maxHeight, image):
+    widthRatio = maxWidth / image.size[0]
+    heightRatio = maxHeight / image.size[1]
+    newWidth = int(widthRatio * image.size[0])
+    newHeight = int(heightRatio * image.size[1])
+    newImage = image.resize((newWidth, newHeight))
+    return newImage
+
+def clear(text):
+    list = text.split(" ")
+    title = ""
+    for i in list:
+        if len(title) + len(i) < 60:
+            title += " " + i
+    return title.strip()
+
+def random_color():
+    return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+
+def predefined_color():
+    colors = [
+        (255, 0, 0),
+        (255, 255, 255),
+        (0, 0, 255),
+        (255, 255, 0),
+        (0, 255, 0),
+        (255, 105, 180),
+        (128, 0, 128)
+    ]
+    return random.choice(colors)
+
+async def gen_thumb(videoid: str):
+    if os.path.isfile(f"cache/{videoid}.png"):
+        return f"cache/{videoid}.png"
+
+    url = f"https://www.youtube.com/watch?v={videoid}"
+    try:
+        results = VideosSearch(url, limit=1)
+        for result in (await results.next())["result"]:
+            try:
+                title = result["title"]
+                title = re.sub("\W+", " ", title)
+                title = title.title()
+            except:
+                title = "Unsupported Title"
+            try:
+                duration = result["duration"]
+            except:
+                duration = "Unknown Mins"
+            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+            try:
+                views = result["viewCount"]["short"]
+            except:
+                views = "Unknown Views"
+            try:
+                channel = result["channel"]["name"]
+            except:
+                channel = "Unknown Channel"
+            try:
+                upload_date = result["publishedTime"]
+            except:
+                upload_date = "Unknown Date"
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(thumbnail) as resp:
+                if resp.status == 200:
+                    f = await aiofiles.open(f"cache/thumb{videoid}.png", mode="wb")
+                    await f.write(await resp.read())
+                    await f.close()
+
+        youtube = Image.open(f"cache/thumb{videoid}.png")
+        blurred_thumbnail = youtube.filter(ImageFilter.GaussianBlur(7))
+        icon_path = "ClassyMusic/ISTKHAR/tt.png"
+        icon = Image.open(icon_path)
+        icon_size = (850, 800)
+        icon = icon.resize(icon_size)
+        thumbnail_width, thumbnail_height = blurred_thumbnail.size
+        icon_width, icon_height = icon.size
+        offset_left = 0
+        offset_right = 0
+        offset_up = 0
+        offset_down = 0
+        
+        icon_position = (
+        (thumbnail_width - icon_width) // 2 + offset_right - offset_left,
+        (thumbnail_height - icon_height) // 2 + offset_down - offset_up
+    )
+        blurred_thumbnail.paste(icon, icon_position, icon.convert('RGBA').split()[3])
+        original_thumbnail = youtube.resize((215, 170))
+        original_with_border = Image.new("RGBA", original_thumbnail.size, (0, 0, 0, 0))
+        original_with_border.paste(original_thumbnail, (0, 0), original_thumbnail.convert('RGBA').split()[3])
+        original_offset_left = 165
+        original_offset_right = 0
+        original_offset_up = 100
+        original_offset_down = 0
+
+        original_position = (
+        (thumbnail_width - original_with_border.width) // 2 + original_offset_right - original_offset_left, 
+        (thumbnail_height - original_with_border.height) // 2 + original_offset_down - original_offset_up
+    )
+        blurred_thumbnail.paste(original_with_border, original_position)
+        try:
+            font = ImageFont.truetype("ClassyMusic/ISTKHAR/f.ttf", 20)
+        except IOError:
+            font = ImageFont.load_default()
+        draw = ImageDraw.Draw(blurred_thumbnail)
+        title_words = title.split()[:6] 
+        truncated_title = ' '.join(title_words)
+        draw.text((600, 190), f"Uɴᴛᴏʟᴅ", font=font, fill=predefined_color())
+        draw.text((600, 220), f"{truncated_title}", font=font, fill=(255, 255,255))
+        draw.text((600, 260), f"{channel}", font=font, fill=(255, 255,255))
+        try:
+            os.remove(f"cache/thumb{videoid}.png")
+        except:
+            pass
+        blurred_thumbnail.save(f"cache/{videoid}.png")
+        return f"cache/{videoid}.png"
+    except Exception as e:
+        print(e)
+        return YOUTUBE_IMG_URL
